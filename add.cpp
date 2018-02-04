d2fecf7dbb838ef6d0c1090541e98566bbdc36a7
Lukasz Kaminski <atakiego@tutanota.com>
2018-02-04 17:45:43 +0100

    added test file

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include "GUI/Simple_window.h"
+#include "GUI/Graph.h"
+
+int main()
+{
+    using namespace Graph_lib;
+    Point tl(1200,250);
+    Simple_window win(tl,600,400,"Canvas");
+
+    Rectangle r{Point{100,200},50,30};
+    win.attach(r);
+
+    win.set_label("Okno");
+    win.wait_for_button();
+}
